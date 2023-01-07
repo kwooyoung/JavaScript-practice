e9d740078eef5d4e02242d220e072abac469be43
wooyoung kim <wy1615@naver.com>
2023-01-07 19:19:50 +0900

    addEventListener로 함수를 호출함으로써 이떄 js는 event object를 인수(argument)로 주고 있고, 실행되는 함수에 인수값을 넣으면 그 오브젝트의 정보를 받을 수 있다. 정보를 받기 원하지 않으면 비워두면 된다. -nomadcoders

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input"); 
-const loginButton = document.querySelector("#login-form button");
-/*
-const loginForm = document.getElementById("login-form");
-const loginInput = document.querySelector("input"); 
-const loginButton = document.querySelector("button");
-이렇게도 사용 가능
-*/
-function loginBtnClick(){
-    const userName = loginInput.value;
-    console.log(userName);
-    
+
+
+function onLoginSubmit(event){
+    event.preventDefault(); //preventDefault 함수는 어떤 event의 브라우저의 기본행동이든지 발생되지 않도록 막는 것이다. 
+    console.log(loginInput.value);
 }
-loginButton.addEventListener('click', loginBtnClick);
+loginForm.addEventListener("submit",onLoginSubmit);
+/*
+ onLoginSubmit()브라우저는 위에서 submit 이벤트가 발생하면 onLoginSubmit()함수를 실행하기도 하지만,
+onLoginSubmit() 브라우저는 브라우저 내에서 event로부터 정보를 잡아내서 
+이 argument 안에 정보를 주고 있다. 
+event가 발생할 때 브라우저가 내 function을 호출하게 되는데, onLoginSubmit() 이렇게 비어있는 채로 호출하지 않고,
+첫번째 argument로써 추가적인 정보를 가진 채 호출하게 된다. 어떤 정보를 브라우저가 주고 있는지 
+보는 방법은 위 함수를 이렇게 변경해보면 된다.
+
+function jsInfo(info){
+    event.preventDefault();
+    console.log(info);
+} 
+javascript는 EventLisener에 어떤 function을 추가하던 간에 첫번째 argument(info)로 발생된
+event에 대한 정보를 준다. 받고 싶지 않으면 비워두면 된다.
+
+loginForm.addEventListener("submit",jsInfo);
+
+모든 EventListener fuction의 첫번째 argument는 항상 발생한 일에 대해 필요로 할만한 정보들을 준다.
 
 
+*/
 
