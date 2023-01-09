0ba9d2e0cc17a97d2b2d0ef7fa0739305871c43b
wooyoung kim <wy1615@naver.com>
2023-01-10 04:57:04 +0900

    createElement 사용으로 javascript -> html element 생성, document.body.appendChild()를 통하여 randoum background구현; -nomadcoders

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images=[
+    "0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"
+];
+
+ const chosenImg = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImg}`;
+document.body.appendChild(bgImage);
