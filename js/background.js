bf4d1c2e1cd634d5323bf5e31ad4496b961d8cae
wooyoung kim <wy1615@naver.com>
2023-01-10 23:27:22 +0900

    javascript를 통한 html tag 생성기능(createElement,appendChild)을 이용하여 ToDo List 생성완료, click event로 부모의 element remove 기능 생성 (event.target.parentElement, 투두리스트 제거 기능 생성) -nomadcoders

--- js/background.js
+++ js/background.js
@@ -1,10 +1,23 @@
 const images=[
-    "0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"
+    "0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg"
 ];
 
  const chosenImg = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img");  
+//console(bgImage);<img> 생성 
 
-bgImage.src = `img/${chosenImg}`;
-document.body.appendChild(bgImage);
+bgImage.src = `img/${chosenImg}`;//img src = "img/*.jpg"
+document.body.appendChild(bgImage);//body맨 마지막에 에 생성
+/*
+document.body.prepend(bgimage); 가장위에 추가하고 싶으면 prepend
+
+appendChild를 사용할 때 body원하는 위치에 넣고 싶으면,
+insertBefore() 메소드를 사용,
+참조된 노드 앞에 특정 부모 노드의 자식 노드를 삽입
+const h2 = document.querySelector("#clock");
+document.body.insertBefore(bgImage, h2);
+두개의 인자중에 앞의 bgImage는 새로운 노드 즉 추가하고 
+싶은 노드이고 h2는 참조할 노드입니다. 
+즉 이렇게 코드를 짜시면 h2요소 앞에 bgImage가 추가되게 된다.
+*/
---
+++ js/todo.js
@@ -0,0 +1,31 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = toDoForm.querySelector("input");
+//const toDoInput = document.getElementById("#todo-form input");
+
+function deleteToDo(event){ 
+    const li = event.target.parentElement;//click event target에 접근, 그 부모 element인 li를 셀렉
+    li.remove();// 클릭 li remove
+}
+function paintToDo(newTodo){
+   const li = document.createElement("li");
+   const span = document.createElement("span");
+   span.innerText= newTodo;
+   const button = document.createElement("button");
+   button.innerText = '❌';
+   button.addEventListener('click',deleteToDo);
+   li.appendChild(span); //li안에 span옵션 생성 button 생성을 위해 
+   li.appendChild(button);
+   toDoList.appendChild(li); //appendchild toDoList 맨 마지막에 li 생성
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value; //toDoinput 입력값 newToDo 에 저장
+    toDoInput.value=""; //input value 비우기(입력값 비우기) 
+    paintToDo(newTodo);
+}
+toDoForm.addEventListener("submit",handleToDoSubmit);
+/*submit의 기본동작은 페이지를 새로고침하는 것이라서 
+event.preventDefault(); 함수를 통하여 기본동작을 막는다.
+  */
