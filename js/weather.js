3adfc8a04b1ede02ae5f052ecdd04166afa250be
wooyoung kim <wy1615@naver.com>
2023-01-18 20:28:43 +0900

    weather API를 사용하여 사이트에 현재 날씨,온도 구현 및 현재 위치(위도,경도)를 이용하여 사용자의 도시이름 구현 새로 사용한 함수 fetch(), fetch.then(), API에서 알려주는 정보를 이용하여 사이트에 구현 (https://openweathermap.org/current)
    -nomadcoders

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,32 @@
-API_KEY = "41e1bb3cf35ce5a5477995fee85713a8";
-function onGeoOK(position){
-     const lat = position.coords.latitude;
-     const lon = position.coords.longitude;
-     console.log("you live in", lat, lng);
-     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
-    }
-function onGeoError(){
-    alert("can't find yoiu . No weather for you.");
+const API_KEY="41e1bb3cf35ce5a5477995fee85713a8";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric `
+    fetch(url)
+    .then(response => response.json())
+        .then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+        
+    
+    //fetch url을 부름(call) 
+    /* 구글 크롬 개발자도구 network에 가면 Wifi에서 확인하면 
+    javascript에서 url요청한 내역이 보임
+    javascript가 대신 url을 부름 .
+    &units=metric = url 끝에 붙여 화씨 온도를 섭씨로 변경.
+    response.json()은 network에서 알려주는 정보들을 추출
+    .then(date) 추출한 다음 데이터를 가져옴 
+    console.log로 원하는걸 볼 수 있음
+    data.name   = 현재 위치의 도시
+    data.weather[0].main = 현재 날씨, 섭씨 온도를 가리킴
+     */
 }
-
-navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
-/*getCurrentPosition()는 2개의 인수가 필요, 1개는 실행이 잘 되었을때 
-실행할 함수 나머지 하나는 에러가 발생했을 때 실행할 함수.
-이 메서드는 장치의 현재 위치를 가져옴.
-
-*/
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
